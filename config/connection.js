f98019b1f6314ae64b21a9cf0aed8ec0e8d59544
Nick Leon <nicolas7@vt.edu>
2021-09-06 22:45:59 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    heroku_a249381720684fc,
+    b78baa364edbfb,
+    acd5648d,
     {
-      host: 'localhost',
+      host: 'us-cdbr-east-04.cleardb.com',
       dialect: 'mysql',
       port: 3306
     }
